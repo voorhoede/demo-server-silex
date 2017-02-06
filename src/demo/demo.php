44e6d39f3e69a811add9a84ca8d30a081a8553ce
Jasper Moelker <j.b.moelker@activeids.nl>
2017-02-06 09:51:55 +0100

    feat(info): basic readme in info panel

--- src/demo/demo.php
+++ src/demo/demo.php
@@ -6,13 +6,12 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use \Michelf\MarkdownExtra;
 
 $demo = $app['controllers_factory'];
 
 $demo->get('/', function (Request $request) use ($app) {
-    $lang = $request->query->get('lang', 'en');
-    $data = array();
-    return $app['twig']->render('demo/viewer/viewer.html', $data);
+    return $app['twig']->render('demo/viewer/viewer.html', array());
 })
 ->bind('demo');
 
@@ -48,29 +47,56 @@ $demo->get('/mapping.json', function () use ($app) {
 ->bind('demo/mapping.json');
 
 
-$demo->get('/modules', function() use ($app) {
+$demo->get('/modules.json', function() use ($app) {
     $finder = new Finder();
     $files = $finder->in(__DIR__ . '/../')->files()->name('*.demo.html');
     $modules = array();
     foreach ($files as $file) {
         $filename = $file->getRelativePathname();
         $parts = explode('/', $filename);
+
         $item = array(
             'group' => $parts[0],
             'name' => $parts[1],
-            'url' => 'modules/'. $filename,
+            'url' => 'modules/' . $filename,
+            'info' => array()
         );
+
+        $readmeFilename = dirname($filename) . '/README.md';
+        if (file_exists(__DIR__ . '/../' . $readmeFilename)) {
+            $item['info']['readme'] = 'info/' . $readmeFilename; 
+        }
+
         array_push($modules, $item);
     }
     return $app->json($modules);
 })
-->bind('demo/modules');
+->bind('demo/modules.json');
 
 
 $demo->get('/modules/{modulePath}', function ($modulePath) use ($app) {
     return $app['twig']->render($modulePath, array());
 })
 ->assert('modulePath', '.+');
+
+
+$demo->get('/info/{moduleDir}/README.md', function ($moduleDir) use ($app) {
+    $markdown = getContents(__DIR__ . '/../' . $moduleDir . '/README.md');
+    $info = MarkdownExtra::defaultTransform($markdown);
+    return $app['twig']->render('demo/info/info.html', array(
+        'slug' => $moduleDir,
+        'info' => $info,
+    ));
+})
+->assert('moduleDir', '.+');
+
+
+function getContents($filename) {
+    if (file_exists($filename)) {
+        return file_get_contents($filename);
+    }
+    return '';
+}
 
 
 return $demo;
